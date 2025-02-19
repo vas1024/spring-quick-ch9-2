4699802a4728948df8fe0b26d6a8574c68c3dd76
i5nuc <ip14922@gmail.com>
2025-02-19 23:37:07 +0300

    ch 9.2 with session id

--- src/main/java/ch9/controlers/MainController.java
+++ src/main/java/ch9/controlers/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class MainController {
@@ -15,10 +16,11 @@ public class MainController {
 
 
   @GetMapping("/main")
-  public String mainGet( Model model ){
+  public String mainGet( HttpSession httpSession, Model model ){
     String username = loggedUserManagementService.getUsername();
     if( username != null ){
       model.addAttribute("username", username );
+      model.addAttribute("sessionid", httpSession.getId() );
       return "main.html";
     }
     else return "redirect:/";
